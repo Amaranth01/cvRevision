446f81108f8abffb1633ca313245d2c443e923d2
Amaranth01 <vanessa.nectoux@gmail.com>
2022-08-25 15:10:29 +0200

    add Json

--- app.js
+++ app.js
@@ -19,3 +19,40 @@ function changeLetter() {
     let letter = document.querySelectorAll("label");
 
 }
+
+let jsonDataObjectHobbies = `{
+    "listeHobbies": {        
+        "hobbies": [
+            "Les jeux vidéo",
+            "la lecture",
+            "La rédaction",
+            "Le dressage canin"
+        ]
+    },
+}`;
+
+let json = JSON.stringify(jsonDataObjectHobbies);
+
+document.querySelector('#hobbies').innerHTML = json;
+
+let jsonSataObjectSkill = `{
+    "listSkill" : {
+        "ComputerSkill": [
+            "logiciel": "Maîtrise de Wordpress, Gimp, PHPStorm",
+            "Language": "Maîtrise du langage HTML, CSS, JS, PHP, SQL"
+        ]
+    },
+}`;
+
+let jsonSkill = JSON.stringify(jsonSataObjectSkill);
+document.querySelector('#computerSkill').innerHTML = jsonSkill;
+
+let jsonDD = `{
+    "langue": {
+        "anglais": "niveau intermédiaire",
+        "Allemand": "niveau débutant",
+        "Langue des signes": "niveau intermédiaire"
+  }
+}`;
+let jsonLanguage = JSON.stringify(jsonDD);
+document.querySelector('#languageSkill').innerHTML = jsonLanguage;
